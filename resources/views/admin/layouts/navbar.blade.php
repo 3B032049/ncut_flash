aa5060604004688489655c4dd46383426ffd3d06
BEE <darksbike44@gmail.com>
2022-11-12 15:03:31 +0800

    no message

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -42,7 +42,9 @@
                         document.getElementById('logout-form').submit();" >
                         <i class="icon-switch2"></i> 登出
                     </a>
-
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                    <i class="icon-switch2"></i> 回到首頁
+                    </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                         @method('POST')
