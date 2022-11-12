ac220fe3818e4aa96a693b84ff2083a8fb94d065
BEE <darksbike44@gmail.com>
2022-11-13 01:21:04 +0800

    make it better

--- config/database.php
+++ config/database.php
@@ -49,7 +49,6 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '33060'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
